5829ba2cefefeeeec1714e91639cb3d30c2196ad
Joab Urbano <joab.uaraujo@gmail.com>
2023-06-24 16:06:43 -0300

    Documentação doxygen para funcionario.hpp

--- funcionario.hpp
+++ funcionario.hpp
@@ -22,7 +22,20 @@ public:
     void setIngressoEmpresa(Data ingressoEmpresa);
     void setDiasFalta(int diasFalta);
 
+    /**
+     * @brief Metodo virtual de calcular salario para ser implementado nas classes filhas
+     * 
+     * @return float 
+     */
     virtual float calcularSalario() = 0;
+
+    /**
+     * @brief Metodo virtual que recebe como parametro a data de desligamento do funcionario
+     * para ser implementada nas classes filhas
+     * 
+     * @param desligamento 
+     * @return float 
+     */
     virtual float calcularRecisao(Data desligamento) = 0;
 };
 
